66e70a9d55d634d6bc857c27ab6c44e963688944
Kyree henry <ene.henry.eh@gmail.com>
2025-05-22 23:17:32 +0100

    feat: implement BullBoardAuthMiddleware for Basic authentication handling

---
+++ src/core/middlewares/bullBoardAuth.middleware.ts
@@ -0,0 +1,44 @@
+import { Injectable, NestMiddleware } from "@nestjs/common";
+import logger from "core/utils/winston.util";
+import { NextFunction, Request, Response } from "express";
+
+@Injectable()
+export class BullBoardAuthMiddleware implements NestMiddleware {
+
+  constructor(
+
+  ) { }
+
+  async use(req: Request, res: Response, next: NextFunction): Promise<void> {
+
+    const authHeader = req.get('authorization');
+
+    if (!authHeader?.startsWith('Basic ')) {
+      this.sendUnauthorizedResponse(res);
+      return;
+    }
+
+    const encodedCreds = authHeader.split(' ')[1];
+    const decodedCreds = Buffer.from(encodedCreds, 'base64').toString('utf-8');
+    const [username, password] = decodedCreds.split(':');
+
+    try {
+
+
+    } catch (error) {
+      logger.error(`Bull-board login - ${error}`);
+      this.sendUnauthorizedResponse(res);
+      return;
+    }
+
+    next();
+  }
+
+  private sendUnauthorizedResponse(res: Response): void {
+    res.setHeader(
+      'WWW-Authenticate',
+      'Basic realm="Restricted Area", charset="UTF-8"',
+    );
+    res.sendStatus(401);
+  }
+}
