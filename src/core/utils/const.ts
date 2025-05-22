fac3199443673620fad1ca3a744a4c5f0dbb1bb8
Kyree henry <ene.henry.eh@gmail.com>
2025-05-22 22:54:38 +0100

    feat: add user content and notification service interfaces, and define import queues and platforms

--- src/core/utils/const.ts
+++ src/core/utils/const.ts
@@ -1,4 +1,3 @@
-
 export default {
   // Repository Interfaces
   IUSER_REPOSITORY: "IUserRepository",
@@ -6,9 +5,26 @@ export default {
   IUSERROLE_REPOSITORY: "IUserRoleRepository",
   IROLECLAIM_REPOSITORY: "IRoleClaimRepository",
   IUSERLOGIN_REPOSITORY: "IUserLoginRepository",
+  IUSERCONTENT_REPOSITORY: "IUserContentRepository",
+  INOTIFICATION_REPOSITORY: "INotificationRepository",
   ILINKEDACCOUNT_REPOSITORY: "ILinkedAccountRepository",
   IDATAPROTECTIONKEY_REPOSITORY: "IDataProtectionKeyRepository",
 
   // Service Interfaces
   ITOKEN_SERVICE: "ITokenService",
+  INOTIFICATION_SERVICE: "INotificationService",
+
+  BULL_QUEUES: {
+    FACEBOOK_IMPORT: "facebook-import",
+    INSTAGRAM_IMPORT: "instagram-import",
+    TWITTER_IMPORT: "twitter-import",
+  },
+
+  PLATFORMS: {
+    INSTAGRAM: "instagram",
+    FACEBOOK: "facebook",
+    TWITTER: "twitter",
+    YOUTUBE: "youtube",
+    SPOTIFY: "spotify",
+  }
 }
