0ceceb3c67b55401e36f717822e0afa317ec05b6
Kyree henry <ene.henry.eh@gmail.com>
2025-05-27 07:54:59 +0100

    feat: enhance Instagram import functionality with access token validation and error handling

--- src/features/integrations/instagram/import/instagram-import.endpoint.ts
+++ src/features/integrations/instagram/import/instagram-import.endpoint.ts
@@ -1,9 +1,9 @@
 import { Response } from "express";
 import { CommandBus } from "@nestjs/cqrs";
-import { ApiResponse, ApiTags } from "@nestjs/swagger";
-import { Controller, Post, Res, UseGuards } from "@nestjs/common";
+import { ApiBody, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { UserAccoutGuard } from "../../../../core/passport/account.guard";
-
+import { Body, Controller, HttpStatus, Post, Res, UseGuards } from "@nestjs/common";
+import { InstagramImportCommand, InstagramImportRequestModel } from "./instagram-import.handler";
 
 @ApiTags('Integrations')
 @UseGuards(UserAccoutGuard)
@@ -22,12 +22,17 @@ export class InstagramImportController {
   @ApiResponse({ status: 401, description: 'UNAUTHORIZED' })
   @ApiResponse({ status: 400, description: 'BAD_REQUEST' })
   @ApiResponse({ status: 403, description: 'FORBIDDEN' })
+  @ApiBody({ type: InstagramImportRequestModel, required: false })
   public async Import(
+    @Body() model: InstagramImportRequestModel,
     @Res() res: Response
   ): Promise<Response | void> {
 
-    
+    const result = await this.commandBus.execute(new InstagramImportCommand({ model }));
+    if (model.instagramAccessToken) {
+      return res.status(HttpStatus.OK).json({ message: "Instagram import has begun." });
+    }
 
+    return res.status(HttpStatus.OK).json({ message: "Instagram import has begun.", ...result });
   }
-
 }
