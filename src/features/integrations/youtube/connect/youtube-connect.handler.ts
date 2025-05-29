31844f75312cf33168aeed9319779dc0b500abee
E_A <46349404+123AKAk@users.noreply.github.com>
2025-05-29 23:51:50 +0100

    refactor(youtube): update GoogleUserDataModel interface and improve code formatting in YouTube connect handler

--- src/features/integrations/youtube/connect/youtube-connect.handler.ts
+++ src/features/integrations/youtube/connect/youtube-connect.handler.ts
@@ -1,28 +1,32 @@
-import axios from "axios";
-import * as Joi from "joi";
-import { Inject } from "@nestjs/common";
-import configs from "../../../../configs";
-import _const from "../../../../core/utils/const";
-import { Globals } from "../../../../core/globals";
-import logger from "../../../../core/utils/winston.util";
-import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
-import { LinkedAccount } from "../../../../domain/entities/linkedAccount.entity";
-import { HttpContext } from "../../../../core/middlewares/httpContext.middleware";
-import { IUserRepository } from "../../../../domain/repositories/iuser.repository";
-import ApplicationException from "../../../../core/exceptions/application.exception";
-import { mapToYoutubeProfileModel } from "../../../../domain/mappers/youtube.mapper";
-import { DataProtectionKey } from "../../../../domain/entities/dataProtectionKey.entity";
-import { IUserLoginRepository } from "../../../../domain/repositories/irefreshtoken.repository";
-import { GoogleUserDataModel, YoutubeChannelDataModel, YoutubeProfileModel } from "../../../../domain/contracts/youtube.model";
-import { ILinkedAccountRepository } from "../../../../domain/repositories/ilinkedAccount.repository";
-import { IDataProtectionKeyRepository } from "../../../../domain/repositories/idataProtectionKey.repository";
+import axios from 'axios';
+import * as Joi from 'joi';
+import { Inject } from '@nestjs/common';
+import configs from '../../../../configs';
+import _const from '../../../../core/utils/const';
+import { Globals } from '../../../../core/globals';
+import logger from '../../../../core/utils/winston.util';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { LinkedAccount } from '../../../../domain/entities/linkedAccount.entity';
+import { HttpContext } from '../../../../core/middlewares/httpContext.middleware';
+import { IUserRepository } from '../../../../domain/repositories/iuser.repository';
+import ApplicationException from '../../../../core/exceptions/application.exception';
+import { mapToYoutubeProfileModel } from '../../../../domain/mappers/youtube.mapper';
+import { DataProtectionKey } from '../../../../domain/entities/dataProtectionKey.entity';
+import { IUserLoginRepository } from '../../../../domain/repositories/irefreshtoken.repository';
+import {
+  GoogleUserDataModel,
+  YoutubeChannelDataModel,
+  YoutubeProfileModel,
+} from '../../../../domain/contracts/youtube.model';
+import { ILinkedAccountRepository } from '../../../../domain/repositories/ilinkedAccount.repository';
+import { IDataProtectionKeyRepository } from '../../../../domain/repositories/idataProtectionKey.repository';
 
 const BASE_URL = 'https://www.googleapis.com/oauth2/v2';
 
 export class YoutubeConnectQuery {
   model: {
     state: string;
-  }
+  };
 
   constructor(request: Partial<YoutubeConnectQuery> = {}) {
     Object.assign(this, request);
@@ -33,7 +37,7 @@ export class YoutubeConnectCallbackQuery {
   model: {
     code: string;
     state: string;
-  }
+  };
 
   constructor(request: Partial<YoutubeConnectCallbackQuery> = {}) {
     Object.assign(this, request);
@@ -42,35 +46,39 @@ export class YoutubeConnectCallbackQuery {
 
 const youtubeConnectCallbackValidations = Joi.object({
   code: Joi.string().required().messages({ 'any.required': 'Invalid request' }),
-  state: Joi.string().required().messages({ 'any.required': 'Invalid request' }),
+  state: Joi.string()
+    .required()
+    .messages({ 'any.required': 'Invalid request' }),
 });
 
 @CommandHandler(YoutubeConnectQuery)
-export class YoutubeConnectQueryHandler implements ICommandHandler<YoutubeConnectQuery> {
-
+export class YoutubeConnectQueryHandler
+  implements ICommandHandler<YoutubeConnectQuery>
+{
   constructor(
     @Inject(_const.IDATAPROTECTIONKEY_REPOSITORY)
     private readonly dataProtectionKeyRepository: IDataProtectionKeyRepository,
-  ) { }
+  ) {}
 
   public async execute(command: YoutubeConnectQuery): Promise<void> {
-
     const { model } = command;
 
     // expires in 15 minutes
-    const expiresIn = Math.floor(Date.now() / 1000) + configs.Token.expirationTime;
+    const expiresIn =
+      Math.floor(Date.now() / 1000) + configs.Token.expirationTime;
     await this.dataProtectionKeyRepository.createAsync(
       model.state,
-      "", // value is not used
+      '', // value is not used
       HttpContext.user[Globals.ClaimTypes.UserId],
-      expiresIn
+      expiresIn,
     );
   }
 }
 
 @CommandHandler(YoutubeConnectCallbackQuery)
-export class YoutubeConnectCallbackQueryHandler implements ICommandHandler<YoutubeConnectCallbackQuery> {
-
+export class YoutubeConnectCallbackQueryHandler
+  implements ICommandHandler<YoutubeConnectCallbackQuery>
+{
   constructor(
     @Inject(_const.ILINKEDACCOUNT_REPOSITORY)
     private readonly linkedAccountRepository: ILinkedAccountRepository,
@@ -80,29 +88,45 @@ export class YoutubeConnectCallbackQueryHandler implements ICommandHandler<Youtu
     private readonly dataProtectionKeyRepository: IDataProtectionKeyRepository,
     @Inject(_const.IUSER_REPOSITORY)
     private readonly userRepository: IUserRepository,
-  ) { }
+  ) {}
 
-  public async execute(query: YoutubeConnectCallbackQuery)
-    : Promise<{ accessToken: string; expiresIn: number; profile: YoutubeProfileModel }> {
+  public async execute(query: YoutubeConnectCallbackQuery): Promise<{
+    accessToken: string;
+    expiresIn: number;
+    profile: YoutubeProfileModel;
+  }> {
     const { model } = query;
     await youtubeConnectCallbackValidations.validateAsync(model);
-    await this.validateState(model.state);
+    const dataProtectionKey = await this.validateState(model.state);
 
-    const { access_token, refresh_token, expires_in } = await this.fetchToken(model.code);
+    const { access_token, refresh_token, expires_in } = await this.fetchToken(
+      model.code,
+    );
 
     const userData = await this.fetchUserData(access_token);
-    const user = await this.userRepository.getUserByEmailAsync(userData.profile.email);
+    console.log(userData);
 
-    if (!user || user.id !== HttpContext.user[Globals.ClaimTypes.UserId]) {
-      throw new ApplicationException('Prevented: Alduterated Request Received!');
+    const user = await this.userRepository.getUserByIdAsync(
+      dataProtectionKey.userId,
+    );
+
+    if (!user) {
+      throw new ApplicationException('Prevented: User not found!');
     }
 
-    let linkedAccount = await this.linkedAccountRepository.getByPlatformAndEmailAsync(_const.PLATFORMS.YOUTUBE, user.email);
+    let linkedAccount =
+      await this.linkedAccountRepository.getByPlatformAndEmailAsync(
+        _const.PLATFORMS.YOUTUBE,
+        user.email,
+      );
     if (linkedAccount) {
-      linkedAccount.userName = "";
+      console.log(linkedAccount);
+      linkedAccount.userName = '';
       linkedAccount.profileImage = userData.profile.picture;
-      linkedAccount.followersCount = Number.parseInt(userData.channel.items[0].statistics.subscriberCount),
-        linkedAccount.followingCount = 0; // TODO : retreive this 
+      (linkedAccount.followersCount = Number.parseInt(
+        userData.channel.items[0].statistics.subscriberCount,
+      )),
+        (linkedAccount.followingCount = 0); // TODO : retreive this
       linkedAccount.metaData = {
         hd: userData.profile.hd,
         locale: userData.profile.locale,
@@ -113,67 +137,80 @@ export class YoutubeConnectCallbackQueryHandler implements ICommandHandler<Youtu
           desciption: userData.channel.items[0].snippet.description,
           viewCount: userData.channel.items[0].statistics.viewCount,
           videoCount: userData.channel.items[0].statistics.videoCount,
-          thumbthumbnail: userData.channel.items[0].snippet.thumbnails.default.url,
-        }
+          thumbthumbnail:
+            userData.channel.items[0].snippet.thumbnails.default.url,
+        },
       };
       await this.linkedAccountRepository.updateAsync(linkedAccount);
     } else {
-      linkedAccount = await this.linkedAccountRepository.createAsync(new LinkedAccount({
-        platform: _const.PLATFORMS.YOUTUBE,
-        userId: user.id,
-        email: userData.profile.email,
-        externalId: userData.profile.sub,
-        userName: "",
-        profileImage: userData.profile.picture,
-        followersCount: Number.parseInt(userData.channel.items[0].statistics.subscriberCount),
-        followingCount: 0, // TODO : retreive this
-        metaData: {
-          hd: userData.profile.hd,
-          locale: userData.profile.locale,
-          name: userData.profile.name,
-          channel: {
-            id: userData.channel.items[0].id,
-            title: userData.channel.items[0].snippet.title,
-            desciption: userData.channel.items[0].snippet.description,
-            viewCount: userData.channel.items[0].statistics.viewCount,
-            videoCount: userData.channel.items[0].statistics.videoCount,
-            thumbthumbnail: userData.channel.items[0].snippet.thumbnails.default.url,
-          }
-        }
-      }));
+      linkedAccount = await this.linkedAccountRepository.createAsync(
+        new LinkedAccount({
+          platform: _const.PLATFORMS.YOUTUBE,
+          userId: user.id,
+          email: userData.profile.email,
+          externalId: userData.profile.id,
+          userName: '',
+          profileImage: userData.profile.picture,
+          followersCount: Number.parseInt(
+            userData.channel.items[0].statistics.subscriberCount,
+          ),
+          followingCount: 0, // TODO : retreive this
+          metaData: {
+            hd: userData.profile.hd,
+            locale: userData.profile.locale,
+            name: userData.profile.name,
+            channel: {
+              id: userData.channel.items[0].id,
+              title: userData.channel.items[0].snippet.title,
+              desciption: userData.channel.items[0].snippet.description,
+              viewCount: userData.channel.items[0].statistics.viewCount,
+              videoCount: userData.channel.items[0].statistics.videoCount,
+              thumbnail:
+                userData.channel.items[0].snippet.thumbnails.default.url,
+            },
+          },
+        }),
+      );
     }
 
-    let existingAccountLogin = await this.userLoginRepository.getByUserIdAndProvider(
-      user.id,
-      _const.PLATFORMS.YOUTUBE
-    );
+    let existingAccountLogin =
+      await this.userLoginRepository.getByUserIdAndProvider(
+        user.id,
+        _const.PLATFORMS.YOUTUBE,
+      );
 
     if (existingAccountLogin) {
       existingAccountLogin.tokenValue = refresh_token;
       existingAccountLogin.addedDateUtc = new Date();
-      existingAccountLogin.expiryDateUtc = new Date(Date.now() + 100 * 24 * 60 * 60 * 1000);
+      existingAccountLogin.expiryDateUtc = new Date(
+        Date.now() + 100 * 24 * 60 * 60 * 1000,
+      );
       await this.userLoginRepository.updateAsync(existingAccountLogin);
     } else {
       existingAccountLogin = await this.userLoginRepository.createAysnc(
         _const.PLATFORMS.YOUTUBE,
         user.id,
-        "",
-        "",
-        "",
+        '',
+        '',
+        '',
         refresh_token,
-        new Date(Date.now() + 100 * 24 * 60 * 60 * 1000)
+        new Date(Date.now() + 100 * 24 * 60 * 60 * 1000),
       );
     }
 
     return {
       accessToken: access_token,
       expiresIn: expires_in,
-      profile: mapToYoutubeProfileModel(linkedAccount, true)
-    }
+      profile: mapToYoutubeProfileModel(linkedAccount, true),
+    };
   }
 
-  private async fetchToken(code: string)
-    : Promise<{ access_token: string; token_type: string; expires_in: number; refresh_token: string; }> {
+  private async fetchToken(code: string): Promise<{
+    access_token: string;
+    token_type: string;
+    expires_in: number;
+    refresh_token: string;
+  }> {
     try {
       const response = await axios.post(`https://oauth2.googleapis.com/token`, {
         client_secret: configs.youtube.clientSecret,
@@ -190,11 +227,17 @@ export class YoutubeConnectCallbackQueryHandler implements ICommandHandler<Youtu
     }
   }
 
-  private async fetchUserData(accessToken: string): Promise<{ profile: GoogleUserDataModel, channel: YoutubeChannelDataModel }> {
+  private async fetchUserData(accessToken: string): Promise<{
+    profile: GoogleUserDataModel;
+    channel: YoutubeChannelDataModel;
+  }> {
     try {
-      const response = await axios.get<GoogleUserDataModel>(`${BASE_URL}/userinfo`, {
-        headers: { Authorization: `Bearer ${accessToken}` },
-      });
+      const response = await axios.get<GoogleUserDataModel>(
+        `${BASE_URL}/userinfo`,
+        {
+          headers: { Authorization: `Bearer ${accessToken}` },
+        },
+      );
 
       const channelResponse = await axios.get<YoutubeChannelDataModel>(
         'https://www.googleapis.com/youtube/v3/channels',
@@ -206,7 +249,7 @@ export class YoutubeConnectCallbackQueryHandler implements ICommandHandler<Youtu
             part: 'snippet,contentDetails,statistics,brandingSettings',
             mine: 'true',
           },
-        }
+        },
       );
 
       return {
@@ -220,7 +263,8 @@ export class YoutubeConnectCallbackQueryHandler implements ICommandHandler<Youtu
   }
 
   private async validateState(state: string): Promise<DataProtectionKey> {
-    const dataProtectionKey = await this.dataProtectionKeyRepository.getByKeyAsync(state);
+    const dataProtectionKey =
+      await this.dataProtectionKeyRepository.getByKeyAsync(state);
     if (!dataProtectionKey) {
       throw new ApplicationException('Invalid state parameter');
     }
@@ -229,7 +273,7 @@ export class YoutubeConnectCallbackQueryHandler implements ICommandHandler<Youtu
       throw new ApplicationException('State parameter has expired');
     }
 
-    await this.dataProtectionKeyRepository.deleteAsync(dataProtectionKey);
+    // await this.dataProtectionKeyRepository.deleteAsync(dataProtectionKey);
     return dataProtectionKey;
   }
 }
