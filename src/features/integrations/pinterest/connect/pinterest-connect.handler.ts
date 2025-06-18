72f556522a101dcb9339c0cab09ac2ac39f07d34
Kyree henry <ene.henry.eh@gmail.com>
2025-06-18 12:54:09 +0100

    refactor(auth): rename ForgotPassword method to ChangePassword for clarity and consistency refactor(integrations): update expiration logic for Instagram and Pinterest connect handlers

--- src/features/integrations/pinterest/connect/pinterest-connect.handler.ts
+++ src/features/integrations/pinterest/connect/pinterest-connect.handler.ts
@@ -57,7 +57,7 @@ export class PinterestConnectQueryHandler implements ICommandHandler<PinterestCo
     const { model } = command;
 
     // expires in 15 minutes
-    const expiresIn = 15 * 60;
+    const expiresIn = Math.floor(Date.now() / 1000) + configs.Token.expirationTime;
     await this.dataProtectionKeyRepository.createAsync(
       model.state,
       "", // value is not used
@@ -199,7 +199,7 @@ export class PinterestConnectCallbackQueryHandler implements ICommandHandler<Pin
       throw new ApplicationException('Invalid state parameter');
     }
 
-    if (new Date(dataProtectionKey.createdOn.getTime() + dataProtectionKey.expiresIn * 1000) < new Date()) {
+    if (dataProtectionKey.expiresIn < Math.floor(Date.now() / 1000)) {
       throw new ApplicationException('State parameter has expired');
     }
 
