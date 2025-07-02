2e4b554cf127cb386db67d6258395de7351e1bb3
Kyree henry <ene.henry.eh@gmail.com>
2025-07-02 15:35:12 +0100

    feat: Add integration controllers and command handlers for various social platforms

---
+++ src/features/integrations/index.ts
@@ -0,0 +1,66 @@
+export { FacebookConnectController } from "./facebook/connect/facebook-connect.endpoint"
+export { FacebookConnectCallbackQueryHandler, FacebookConnectQueryHandler } from "./facebook/connect/facebook-connect.handler";
+
+export { FacebookImportController } from "./facebook/import/facebook-import.endpoint";
+export { FacebookImportCommandHandler } from "./facebook/import/facebook-import.handler";
+
+export { FacebookProfileController } from "./facebook/get-profile/get-profile.endpoint";
+export { FacebookProfileQueryHandler } from "./facebook/get-profile/get-profile.handler";
+
+export { InstagramConnectController } from "./instagram/connect/instagram-connect.endpoint"
+export { InstagramConnectCallbackQueryHandler, InstagramConnectQueryHandler } from "./instagram/connect/instagram-connect.handler";
+
+export { InstagramProfileController } from "./instagram/get-profile/get-profile.endpoint";
+export { InstagramProfileQueryHandler } from "./instagram/get-profile/get-profile.handler";
+
+export { InstagramImportController } from "./instagram/import/instagram-import.endpoint";
+export { InstagramImportCommandHandler } from "./instagram/import/instagram-import.handler";
+
+export { PinterestConnectController } from "./pinterest/connect/pinterest-connect.endpoint";
+export { PinterestConnectCallbackQueryHandler, PinterestConnectQueryHandler } from "./pinterest/connect/pinterest-connect.handler";
+
+export { PinterestProfileController } from "./pinterest/get-profile/get-profile.endpoint";
+export { PinterestProfileQueryHandler } from "./pinterest/get-profile/get-profile.handler";
+
+export { PinterestImportController } from "./pinterest/import/pinterest-import.endpoint";
+export { PinterestImportCommandHandler } from "./pinterest/import/pinterest-import.handler";
+
+export { RedditConnectController } from "./reddit/connect/reddit-connect.endpoint";
+export { RedditConnectCallbackQueryHandler, RedditConnectQueryHandler } from "./reddit/connect/reddit-connect.handler";
+
+export { RedditProfileController } from "./reddit/get-profile/get-profile.endpoint"
+export { RedditProfileQueryHandler } from "./reddit/get-profile/get-profile.handler"
+
+export { RedditImportController } from "./reddit/import/reddit-import.endpoint";
+export { RedditImportCommandHandler } from "./reddit/import/reddit-import.handler";
+
+export { SpotifyConnectController } from "./spotify/connect/spotify-connect.endpoint";
+export { SpotifyConnectCallbackQueryHandler, SpotifyConnectQueryHandler } from "./spotify/connect/spotify-connect.handler";
+
+export { SpotifyProfileController } from "./spotify/get-profile/get-profile.endpoint";
+export { SpotifyProfileQueryHandler } from "./spotify/get-profile/get-profile.handler";
+
+export { SpotifyImportController } from "./spotify/import/spotify-import.endpoint";
+export { SpotifyImportCommandHandler } from "./spotify/import/spotify-import.handler";
+
+export { TwitterConnectController } from "./twitter/connect/twitter-connect.endpoint";
+export { TwitterConnectCallbackQueryHandler, TwiiterConnectQueryHandler } from "./twitter/connect/twitter-connect.handler";
+
+export { TwitterProfileController } from "./twitter/get-profile/get-profile.endpoint";
+export { TwitterProfileQueryHandler } from "./twitter/get-profile/get-profile.handler";
+
+export { TwitterImportController } from "./twitter/import/twitter-import.endpoint";
+export { TwitterImportCommandHandler } from "./twitter/import/twitter-import.handler";
+
+export { YoutubeConnectController } from "./youtube/connect/youtube-connect.endpoint";
+export { YoutubeConnectCallbackQueryHandler, YoutubeConnectQueryHandler } from "./youtube/connect/youtube-connect.handler";
+
+export { YoutubeProfileController } from "./youtube/get-profile/get-profile.endpoint";
+export { YoutubeProfileQueryHandler } from "./youtube/get-profile/get-profile.handler";
+
+export { YoutubeImportController } from "./youtube/import/youtube-import.endpoint";
+export { YoutubeImportCommandHandler } from "./youtube/import/youtube-import.handler";
+
+export { } from "./";
+export { } from "./";
+export { } from "./";
--- src/infrastructure/services/data.seeder.ts
+++ src/infrastructure/services/data.seeder.ts
@@ -43,8 +43,7 @@ export class DataSeeder {
       emailConfirmed: true,
       type: UserType.Admin,
       createdBy: configs.systemAdmin.email,
-      userName:"",
-
+      userName: "",
     });
 
     if (!(await this.userRepository.getUserByEmailAsync(SYSTEM_ADMIN.email))) {
@@ -75,9 +74,9 @@ export class DataSeeder {
       lastName: configs.guestUser.lastName,
       email: configs.guestUser.email,
       emailConfirmed: true,
-      type: UserType.Guest,
+      type: UserType.User,
       createdBy: configs.systemAdmin.email,
-      userName:""
+      userName: ""
     });
 
     if (!(await this.userRepository.getUserByEmailAsync(JOHN_DOE.email))) {
