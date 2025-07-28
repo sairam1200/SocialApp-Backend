f044ba29fb489107e14b71ac4fb962e541f9db64
Kyree henry <ene.henry.eh@gmail.com>
2025-07-28 13:09:21 +0100

    chore: updated auth module

--- src/modules/auth.module.ts
+++ src/modules/auth.module.ts
@@ -3,12 +3,12 @@ import { JwtService } from '@nestjs/jwt';
 import _const from '../core/utils/const';
 import { CqrsModule } from '@nestjs/cqrs';
 import { EmailModule } from './email.module';
+import authentication from '../features/auth';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthGuardsModule } from './authGuard.module';
 import { dependency } from '../infrastructure/dependency';
 import { NotificationModule } from './notification.module';
 import { UserClaim, User, Role, UserRole, RoleClaim, UserLogin, LinkedAccount, DataProtectionKey } from '../domain/entities';
-import { GoogleAuthenticationController, GoogleConnectQueryHandler, GoogleConnectCallbackQueryHandler, FacebookAuthenticationController, FacebookConnectQueryHandler, FacebookConnectCallbackQueryHandler, ResetPasswordController, ResetPasswordCommandHandler, RegisterController, RegisterCommandHandler, LoginController, LoginCommandHandler, RefreshTokenController, RefreshTokenCommandHandler, Verfiy2FACommandHandler, Enable2FACommandHandler, Setup2FACommandHandler, Disbale2FACommandHandler, Setup2FAController, Enable2FAController, Verify2FAController, Disable2FAController } from '../features/auth';
 
 @Module({
   imports: [
@@ -29,37 +29,19 @@ import { GoogleAuthenticationController, GoogleConnectQueryHandler, GoogleConnec
   ],
   providers: [
     JwtService,
-    LoginCommandHandler,
-    Verfiy2FACommandHandler,
-    Enable2FACommandHandler,
-    Setup2FACommandHandler,
-    Disbale2FACommandHandler,
-    RegisterCommandHandler,
-    RefreshTokenCommandHandler,
-    ResetPasswordCommandHandler,
-    GoogleConnectQueryHandler,
-    GoogleConnectCallbackQueryHandler,
-    FacebookConnectQueryHandler,
-    FacebookConnectCallbackQueryHandler,
+
+    ...authentication.addHandlers(),
+
     dependency.TokenService,
     dependency.UserRepository,
     dependency.RoleRepository,
     dependency.UserRoleRepository,
     dependency.UserLoginRepository,
-    dependency.DataProtectionKeyRepository,
     dependency.LinkedAccountRepository,
+    dependency.DataProtectionKeyRepository,
   ],
   controllers: [
-    LoginController,
-    RegisterController,
-    GoogleAuthenticationController,
-    FacebookAuthenticationController,
-    RefreshTokenController,
-    ResetPasswordController,
-    Setup2FAController,
-    Enable2FAController,
-    Verify2FAController,
-    Disable2FAController
+    ...authentication.addControllers()
   ],
   exports: [],
 })
