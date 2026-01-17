dba4ddd4a9b212cf9a194fc4a7fc76f90097f325
Kyree henry <ene.henry.eh@gmail.com>
2026-01-17 08:21:54 +0100

    update

--- src/infrastructure/repositories/userContent.repository.ts
+++ src/infrastructure/repositories/userContent.repository.ts
@@ -76,8 +76,8 @@ export class UserContentRepository implements IUserContentRepository {
           content.title ILIKE :searchQuery
           OR EXISTS (
             SELECT 1
-            FROM jsonb_each_text(content."metaData") AS kv(key, value)
-            WHERE value ILIKE :searchQuery
+            FROM LATERAL json_each_text(content."metaData") AS kv(key, value)
+            WHERE kv.value ILIKE :searchQuery
           )
         )
       `);
