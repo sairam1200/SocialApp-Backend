6b30e5542c6c7ce61c59be8e2eccbf959d92437e
Sai Ram Chikkala <sairamch10@gmail.com>
2026-06-24 16:40:59 +0530

    Oauth youtube fetch fix updated

--- src/modules/auth.module.ts
+++ src/modules/auth.module.ts
@@ -8,7 +8,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthGuardsModule } from './authGuard.module';
 import { dependency } from '../infrastructure/dependency';
 import { NotificationModule } from './notification.module';
-import { UserClaim, User, Role, UserRole, RoleClaim, UserLogin, LinkedAccount, DataProtectionKey, UserBiometric } from '../domain/entities';
+import { UserClaim, User, Role, UserRole, RoleClaim, UserLogin, LinkedAccount, DataProtectionKey, UserBiometric, YoutubeAccount } from '../domain/entities';
 
 @Module({
   imports: [
@@ -26,6 +26,7 @@ import { UserClaim, User, Role, UserRole, RoleClaim, UserLogin, LinkedAccount, D
       DataProtectionKey,
       LinkedAccount,
       UserBiometric,
+      YoutubeAccount,
     ]),
   ],
   providers: [
@@ -40,6 +41,7 @@ import { UserClaim, User, Role, UserRole, RoleClaim, UserLogin, LinkedAccount, D
     dependency.UserLoginRepository,
     dependency.LinkedAccountRepository,
     dependency.DataProtectionKeyRepository,
+    dependency.YoutubeAccountRepository,
   ],
   controllers: [
     ...authentication.addControllers()
