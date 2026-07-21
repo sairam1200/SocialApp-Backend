9435642891f48b10bfaea2bf7ab2c78f9b464665
Sai Ram Chikkala <sairamch10@gmail.com>
2026-07-21 23:05:35 +0530

    update/buildFix

--- src/main.ts
+++ src/main.ts
@@ -54,6 +54,7 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   app.enableVersioning({
     type: VersioningType.URI,
+    defaultVersion: '1',  
   });
 
   if (configs.env !== 'production') {
